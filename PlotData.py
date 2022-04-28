fc2c68414f7d97b8979e3d811e07e5c4a7e049c3
Gottfrid Olsson <gottfridolsson@hotmail.com>
2022-04-28 09:47:42 +0200

    update

--- PlotData.py
+++ PlotData.py
@@ -160,7 +160,7 @@ def plot_errorbar(ax, xData, yData, xError, yError, errorbarSize, errorbarLinewi
 ##----------##
 
 #temp # OBS! must fill in JSON_readFilePath as of now #tofix!
-readJSONFilePathStringTEMP = "S13_through_S23_time_Xpos" #"20220421_0812_sizeOfAtomicNucleus"
+readJSONFilePathStringTEMP = "S_measurements_time_Xvel_sigma5" #"20220421_0812_sizeOfAtomicNucleus"
 #"IckeOptimeradeTriggers_300V"  #"CONFIG" #"20220406_2122_testdataKandidat" # #"20220222_1014_fluorescenceNormalisedPeak628nmAndSimulation" # "20220221_1934_HeBroadAndGauss2" #"20220223_1558_absorbanceMeanAndSimulation" #  #"20220221_2000_absorption_I2_measurement2" #"20220221_1942_fluorescens_mean"
 
 JSON_readFilePath = "JSON/"+ readJSONFilePathStringTEMP + ".json" #make it such that you can ask for what file it is or smht//2022-02-18
