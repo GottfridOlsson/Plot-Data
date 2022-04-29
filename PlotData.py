b796cdd0f4dc07e5d868799e29b647a85986e5f8
Gottfrid Olsson <gottfridolsson@hotmail.com>
2022-04-29 14:25:26 +0200

    I_allCoils

--- PlotData.py
+++ PlotData.py
@@ -161,7 +161,7 @@ def plot_errorbar(ax, xData, yData, xError, yError, errorbarSize, errorbarLinewi
 
 #temp # OBS! must fill in JSON_readFilePath as of now #tofix!
 
-readJSONFilePathStringTEMP = "processedSimulatedSdata_IallCoils_nonCorrected"
+readJSONFilePathStringTEMP = "processedSimulatedSdata_IallCoils_sameKfactor6"
 #"I_allCoils_nonCorrected"
 #"I_individualCoils_20220428_correctedCurrent"
 
