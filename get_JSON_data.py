b065f08a256fb866d8d308cb2a8439fe66c821a1
T00KER <gottfridolsson@hotmail.com>
2022-06-18 19:12:11 +0200

    Refactoring: get JSON data

--- get_JSON_data.py
+++ get_JSON_data.py
@@ -3,42 +3,102 @@
 ##        File: get_JSON_data.py
 ##      Author: GOTTFRID OLSSON 
 ##     Created: 2022-06-17, 11:54
-##     Updated: 2022-06-17, 11:56
+##     Updated: 2022-06-18, 19:09
 ##       About: Reads and stores standard settings if
 ##              not overwritten by user data in JSON
 ##====================================================##
+
+# Q. that remains: 
+# https://stackoverflow.com/questions/19993795/how-would-i-access-variables-from-one-class-to-another
 
 
 import JSON_handler as JSON
 
 
+##### TODO: fix this s.t. one gets a prompt and pop-up window from which user can select JSON-file
+
+readJSONFilePathStringTEMP = "CONFIG"
 JSON_readFilePath = "JSON/"+ readJSONFilePathStringTEMP + ".json" #make it such that you can ask for what file it is or smht//2022-02-18
-config = read_JSON(JSON_readFilePath)
-c = config
+J = JSON.read(JSON_readFilePath) #Jason-file
 
-
-
-
-#### FROM OLD: ####
 
 
 #--------------------#
 # GET DATA FROM JSON #
 #--------------------#
 
-fig_height      = c['figure_height']
-fig_width       = c['figure_width']
-#fontFamily      = c['font_family'] # #2022-04-07, keeping it in case i want to implement this in the future for other fonts
-#fontDirectory   = c['font_directory'] # #2022-04-07
-LaTeX_and_CMU   = c['LaTeX_and_CMU']
-defaultFontSize = c['fontSize_axis']
-xTickSize       = c['fontSize_tick']
-yTickSize       = c['fontSize_tick']
-legendFontSize  = c['fontSize_legend']
-num_subplots    = c['num_subplots']
-subplots_x      = c['num_subplots_x']
-subplots_y      = c['num_subplots_y']
-max_yDatasets   = c['max_yDatasets']
+filename_csv            = J['filename']['csv']
+filename_pdf            = J['filename']['pdf']
+
+figure_height           = J['figure_size']['height_cm'] # [cm]
+figure_width            = J['figure_size']['width_cm']  # [cm]
+# textwidth_external    = J['figure_size']['textwidth'] # [cm]
+
+font_size_axis          = J['font_size']['axis']   # [pt]
+font_size_tick          = J['font_size']['tick']   # [pt]
+font_size_legend        = J['font_size']['legend'] # [pt]
+# font_size_external    = J['font_size']['external'] # [pt]
+
+LaTeX_and_CMU           = J['LaTeX_and_CMU']
+
+subplot_setup_x_cols    = J['subplot_setup']['x']
+subplot_setup_y_cols    = J['subplot_setup']['y']
+subplot_setup_subplots  = J['subplot_setup']['total_subplots'] #TODO: raise error if "total != x*y"
+
+
+## DECLARE VARIABLE NAMES ##
+
+subplots = range(subplot_setup_subplots)
+
+subplot_num            = [i for i in subplots]
+plot_type              = []
+
+dataset_label          = []
+dataset_col_x          = []
+dataset_col_y          = []
+
+line_color             = []
+line_style             = []
+line_width             = []
+
+marker_type            = []
+marker_size            = []
+marker_thickness       = []
+marker_facecolor       = []
+
+errorbar_on            = []
+errorbar_constant_on   = []
+errorbar_constant_x_pm = []
+errorbar_constant_y_pm = []
+errorbar_x_col         = []
+errorbar_y_col         = []
+errorbar_size          = []
+errorbar_linewidth     = []
+errorbar_capthickness  = []
+
+axis_x_label           = []
+axis_x_limit_min       = []
+axis_x_limit_max       = []
+axis_x_scale           = []
+axis_x_invert          = []
+axis_x_float_precision = []
+
+axis_y_label           = []
+axis_y_limit_min       = []
+axis_y_limit_max       = []
+axis_y_scale           = []
+axis_y_invert          = []
+axis_y_float_precision = []
+
+legend_on              = []
+legend_alpha           = []
+legend_location        = []
+
+grid_major_on          = []
+grid_major_linewidth   = []
+grid_minor_on          = []
+grid_minor_linewidth   = []
+
 
 
 
@@ -46,53 +106,62 @@ max_yDatasets   = c['max_yDatasets']
 # ASSIGN VALUES #
 #---------------#
 
-for i in range(0, num_subplots):
-    datasets_per_subplot[i]   = c['subplots'][i]['num_yDatasets']
-    xLabel[i]                 = c['subplots'][i]['xLabel']
-    yLabel[i]                 = c['subplots'][i]['yLabel']
-    xScale[i]                 = c['subplots'][i]['xScale']
-    yScale[i]                 = c['subplots'][i]['yScale']
-    xlim_min[i]               = c['subplots'][i]['xlim_min']
-    xlim_max[i]               = c['subplots'][i]['xlim_max']
-    ylim_min[i]               = c['subplots'][i]['ylim_min']
-    ylim_max[i]               = c['subplots'][i]['ylim_max']
-    floatPrec_xAxis[i]        = c['subplots'][i]['floatPrec_xAxis']
-    floatPrec_yAxis[i]        = c['subplots'][i]['floatPrec_yAxis']
-    legendOn[i]               = c['subplots'][i]['legend_on']
-    legendLocation[i]         = c['subplots'][i]['legend_location']
-    legendAlpha[i]            = c['subplots'][i]['legend_alpha']
-    gridMajorOn[i]            = c['subplots'][i]['grid_on']
-    gridMinorOn[i]            = c['subplots'][i]['grid_minor_on']
+###### TODO: do this for standard JSON-values
+###### TODO: figure out how to "check" if a value in JSON exists (if not, don't overwrite standard values; if exists, do write over)
+ 
 
-    for k in range(0, c['subplots'][i]['num_yDatasets']):
-        subplot_xCol[i][k]    = c['subplots'][i]['xDataCol'][k][str(k+1)] - 1
-        subplot_yCol[i][k]    = c['subplots'][i]['yDataCol'][k][str(k+1)] - 1
-        plot_type[i][k]       = c['subplots'][i]['yDataset'][k]['plot_type']
-        dataLabel[i][k]       = c['subplots'][i]['yDataset'][k]['datalabel']
-        lineColor[i][k]       = c['subplots'][i]['yDataset'][k]['line_color']
-        lineStyle[i][k]       = c['subplots'][i]['yDataset'][k]['line_style']
-        lineWidth[i][k]       = c['subplots'][i]['yDataset'][k]['line_width']
-        markerType[i][k]      = c['subplots'][i]['yDataset'][k]['marker_type']
-        markerSize[i][k]      = c['subplots'][i]['yDataset'][k]['marker_size']
-        markerThickness[i][k] = c['subplots'][i]['yDataset'][k]['marker_thickness']
-        markerFacecolor[i][k] = c['subplots'][i]['yDataset'][k]['marker_facecolor']
-        
-        if plot_type[i][k] == "errorbar":
-            errorbarSize[i][k]          = c['subplots'][i]['yDataset'][k]['errorbar_size']
-            errorbarLinewidth[i][k]     = c['subplots'][i]['yDataset'][k]['errorbar_linewidth']
-            errorbarCapthickness[i][k]  = c['subplots'][i]['yDataset'][k]['errorbar_capthickness']
-            if c['subplots'][i]['yDataset'][k]['constant_errorbar']:
-                xError[i][k]            = c['subplots'][i]['yDataset'][k]['constant_xError']
-                yError[i][k]            = c['subplots'][i]['yDataset'][k]['constant_yError']
-            else:
-                errorbar_xError_col     = c['subplots'][i]['yDataset'][k]['errorbar_xError_col']
-                errorbar_yError_col     = c['subplots'][i]['yDataset'][k]['errorbar_yError_col']
-                if errorbar_xError_col is not False:
-                    xError[i][k] = data[header[errorbar_xError_col]]
-                else:
-                    xError[i][k] = 0 # can i make this more effective, coding-wise?
+## PER SUBPLOT ##
 
-                if errorbar_yError_col is not False:
-                    yError[i][k] = data[header[errorbar_yError_col]]
-                else:
-                    yError[i][k] = 0 # can i make this more effective, coding-wise?
+for i in subplots:
+    plot_type.append(               J['subplot_settings'][i]['datasets']['plot_type']                                   )
+    dataset_label.append(           J['subplot_settings'][i]['datasets']['dataset_label']                               )  
+
+    dataset_col_x.append(           J['subplot_settings'][i]['datasets']['column_x']                                        )
+    dataset_col_y.append(           J['subplot_settings'][i]['datasets']['column_y']                                        )
+
+    axis_x_label.append(            J['subplot_settings'][i]['datasets']['axis']['x']['label']                              )
+    axis_x_limit_min.append(        J['subplot_settings'][i]['datasets']['axis']['x']['limit']['min']                       )
+    axis_x_limit_max.append(        J['subplot_settings'][i]['datasets']['axis']['x']['limit']['max']                       )
+    axis_x_scale.append(            J['subplot_settings'][i]['datasets']['axis']['x']['scale']                              )
+    axis_x_invert.append(           J['subplot_settings'][i]['datasets']['axis']['x']['invert']                             )
+    axis_x_float_precision.append(  J['subplot_settings'][i]['datasets']['axis']['x']['float_precision']                    )   
+
+    axis_y_label.append(            J['subplot_settings'][i]['datasets']['axis']['y']['label']                              )
+    axis_y_limit_min.append(        J['subplot_settings'][i]['datasets']['axis']['y']['limit']['min']                       )
+    axis_y_limit_max.append(        J['subplot_settings'][i]['datasets']['axis']['y']['limit']['max']                       )
+    axis_y_scale.append(            J['subplot_settings'][i]['datasets']['axis']['y']['scale']                              )
+    axis_y_invert.append(           J['subplot_settings'][i]['datasets']['axis']['y']['invert']                             )
+    axis_y_float_precision.append(  J['subplot_settings'][i]['datasets']['axis']['y']['float_precision']                    )
+
+    legend_on.append(               J['subplot_settings'][i]['datasets']['legend']['on']                                    )
+    legend_alpha.append(            J['subplot_settings'][i]['datasets']['legend']['alpha']                                 )
+    legend_location.append(         J['subplot_settings'][i]['datasets']['legend']['location']                              )   
+
+    grid_major_on.append(           J['subplot_settings'][i]['datasets']['grid']['major']['on']                             )
+    grid_major_linewidth.append(    J['subplot_settings'][i]['datasets']['grid']['major']['linewidth']                      )
+    grid_minor_on.append(           J['subplot_settings'][i]['datasets']['grid']['minor']['on']                             )    
+    grid_minor_linewidth.append(    J['subplot_settings'][i]['datasets']['grid']['minor']['linewidth']                      )
+
+    # TODO: "if":s that select what 'plot_type_settings' to get from JSON (line, marker, errorbar, ...)
+    line_color.append(              J['subplot_settings'][i]['datasets']['plot_type_settings']['line']['color']             )
+    line_style.append(              J['subplot_settings'][i]['datasets']['plot_type_settings']['line']['style']             )
+    line_width.append(              J['subplot_settings'][i]['datasets']['plot_type_settings']['line']['width']             )
+
+    marker_type.append(             J['subplot_settings'][i]['datasets']['plot_type_settings']['marker']['type']            )
+    marker_size.append(             J['subplot_settings'][i]['datasets']['plot_type_settings']['marker']['size']            )
+    marker_thickness.append(        J['subplot_settings'][i]['datasets']['plot_type_settings']['marker']['thickness']       )
+    marker_facecolor.append(        J['subplot_settings'][i]['datasets']['plot_type_settings']['marker']['facecolor']       )
+
+    errorbar_on.append(             J['subplot_settings'][i]['datasets']['plot_type_settings']['errorbar']['on']            )
+    errorbar_x_col.append(          J['subplot_settings'][i]['datasets']['plot_type_settings']['errorbar']['x_col']         )
+    errorbar_y_col.append(          J['subplot_settings'][i]['datasets']['plot_type_settings']['errorbar']['y_col']         )
+    errorbar_size.append(           J['subplot_settings'][i]['datasets']['plot_type_settings']['errorbar']['size']          )
+    errorbar_linewidth.append(      J['subplot_settings'][i]['datasets']['plot_type_settings']['errorbar']['linewidth']     )
+    errorbar_capthickness.append(   J['subplot_settings'][i]['datasets']['plot_type_settings']['errorbar']['capthickness']  )
+    errorbar_constant_on.append(    J['subplot_settings'][i]['datasets']['plot_type_settings']['errorbar']['constant']['on']    )
+    errorbar_constant_x_pm.append(  J['subplot_settings'][i]['datasets']['plot_type_settings']['errorbar']['constant']['x_pm']  )
+    errorbar_constant_y_pm.append(  J['subplot_settings'][i]['datasets']['plot_type_settings']['errorbar']['constant']['y_pm']  )
+
+
+
+print("DONE: get_JSON_data.py")
--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 ##        File: main.py
 ##      Author: GOTTFRID OLSSON 
 ##     Created: 2022-06-17, 10:56
-##     Updated: 2022-06-17, 11:56
+##     Updated: 2022-06-18, 119:09
 ##       About: Plot data from CSV with matplotlib.
 ##              Plot-settings in JSON, export as PDF.
 ##====================================================##
@@ -19,8 +19,8 @@ import matplotlib
 import matplotlib.pyplot as plt         # to plot
 
  #import JSON_handler as JSON
-import get_JSON_data as JSON
-import CSV_handler as CSV
+#import get_JSON_data as JSON
+#import CSV_handler as CSV
 
 import errorbar
 
@@ -29,3 +29,29 @@ import errorbar
 #------------#
 #    MAIN    #
 #------------#
+
+
+if __name__ == "__main__":
+    print("---Program Start---")
+
+
+    #for i in subplots:
+        #if plot_type[i] == "errorbar":
+        #   errorbar.plot_errorbar(**Args) 
+        # and so on for all IMPLEMENTED plot_types
+
+
+
+  #  if LaTeX_and_CMU:
+  #      plt.rcParams.update({
+  #      "text.usetex": True,
+  #      "font.family": "serif", 
+  #      "font.serif" : ["Computer Modern Roman"]
+  #  })
+
+
+
+
+
+    print("---Program End---")
+    
