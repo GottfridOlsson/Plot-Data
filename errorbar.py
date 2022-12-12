98f1496c0a4105c62911fc0f70bfa99829d6eaf8
Gottfrid Olsson <gottfridolsson@hotmail.com>
2022-12-12 09:24:06 +0100

    Update errorbar ecolor

--- errorbar.py
+++ errorbar.py
@@ -3,7 +3,7 @@
 ##        File: errorbar.py
 ##      Author: GOTTFRID OLSSON 
 ##     Created: 2022-06-17
-##     Updated: 2022-12-09
+##     Updated: 2022-12-12
 ##       About: Plot data with errorbars.
 ##====================================================##
 
@@ -11,7 +11,7 @@
 
 ## FUNCTIONS ##
 
-def plot_errorbar(ax, data_x, data_y, errorbar_on, errorbar_x, errorbar_y, errorbar_size, errorbar_linewidth, errorbar_capthickness, data_label, line_color, line_style, line_width, marker_type, marker_size, marker_thickness, marker_facecolor, ax_num, ecolor):
+def plot_errorbar(ax, data_x, data_y, errorbar_on, errorbar_x, errorbar_y, errorbar_size, errorbar_linewidth, errorbar_capthickness, data_label, line_color, line_style, line_width, marker_type, marker_size, marker_thickness, marker_facecolor, ax_num, ecolor=None):
     if errorbar_on:  
         out = ax.errorbar(data_x, data_y, label=data_label, color=line_color, linestyle=line_style, linewidth=line_width, \
             marker=marker_type, markersize=marker_size, markeredgewidth=marker_thickness, markerfacecolor=marker_facecolor, \
@@ -19,7 +19,7 @@ def plot_errorbar(ax, data_x, data_y, errorbar_on, errorbar_x, errorbar_y, error
         print("DONE: Plotted data with 'errorbar' on axs: " + str(ax_num))
     else:
         out = ax.plot(data_x, data_y, label=data_label, color=line_color, linestyle=line_style, linewidth=line_width, \
-        marker=marker_type, markersize=marker_size, markeredgewidth=marker_thickness, markerfacecolor=marker_facecolor, ecolor=ecolor)
+        marker=marker_type, markersize=marker_size, markeredgewidth=marker_thickness, markerfacecolor=marker_facecolor)
         print("DONE: Plotted data with 'errorbar' (without errorbars) on axs: " + str(ax_num))
     
     return out
