e99b696dbdadaa74d658cbeb548ecc124685f31b
Gottfrid Olsson <gottfridolsson@hotmail.com>
2023-02-08 15:19:28 +0100

    Figures

--- combine_CSV_columnwise.py
+++ combine_CSV_columnwise.py
@@ -1,14 +1,12 @@
 import CSV_handler
 
 # name of output file
-output_string = "TIF320_A1_T4_comparison_helium_wavefunction_T1_T4_T5_T6.csv"
+output_string = "TIF320_A3_RDF_Na-O_our_their.csv"
 
 #names of csv files to get columns from and print to output file
 raw_paths = [
-    "A1_Task1_helium_wavefunction_4gaussians_as_basis_N=1000.csv",
-    "A1_Task4_helium_wavefunction_N=6000_rmax=30_x=False_c=False.csv",
-    "A1_Task5_helium_wavefunction_N=6000_rmax=30_x=True_c=False.csv", 
-    "A1_Task6_helium_wavefunction_N=6000_rmax=30_x=True_c=True.csv"
+    "TIF320_A3_RDF_histogram.csv",
+    "TIF320_A3_RDF_histogram_their_simulation.csv"
 ]
 
 
@@ -19,3 +17,4 @@ formatted_CSV_folder = "CSV\\"
 output_path = formatted_CSV_folder + output_string
 
 CSV_handler.combine_CSV_files_to_one(output_path, formatted_paths)
+print(f"Combined {len(raw_paths)} CSV files to one at path: {output_path}")
