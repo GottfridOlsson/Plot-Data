c05768f4c6acb06457225febf28b6c9b0f8a95f6
Gottfrid Olsson <gottfridolsson@hotmail.com>
2024-01-29 13:31:55 +0100

    plot_reusable update

--- TRA280_coin-cell-lab_figures.py
+++ TRA280_coin-cell-lab_figures.py
@@ -3,7 +3,7 @@
 ##        File: plot_reusable.py
 ##      Author: GOTTFRID OLSSON 
 ##     Created: 2023-05-27
-##     Updated: 2024-01-29
+##     Updated: 2023-05-27
 ##       About: Plot data from CSV with matplotlib.
 ##              1. Read CSV
 ##              2. Do calculations (change this dependent on your case).
@@ -22,8 +22,8 @@ import matplotlib.ticker as plticker
 
 # READ CSV #
 # Change these:
-filename_csv = 'testdata1.csv' 
-filename_pdf = 'testdata1.pdf'
+filename_csv = 'TIF120_LSPR-peak_vs_ethylene-glycol-concentration_fit.csv' 
+filename_pdf = 'TIF120_LSPR-peak_vs_ethylene-glycol-concentration_fit.pdf'
 
 CURRENT_PATH = os.path.abspath(os.path.dirname(__file__))
 CSV_path = CURRENT_PATH + "\\CSV\\" + filename_csv
@@ -36,6 +36,9 @@ CSV_header = CSV.get_header(CSV_data)
 
 # FUNCTIONS #
 
+concentrations = [0, 5, 10, 20, 30, 50] # percent glycol in water (zero means only water, values from lab)
+LSPR_peak_time_ranges = [(70,130), (240,300), (395,455), (545,605), (700,760), (810,870), (1050,1110)] # corresponding times for concentrations (values from plot)
+LSPR_peak_means = []
 
 # DATA ANLYSIS / CALCULATIONS #
 
@@ -51,44 +54,30 @@ fit_y_oneSigma = CSV_data[CSV_header[4]]
 
 
 # PLOT SETTINGS #
-fig_width_cm = 16
-fig_height_cm = 9
-
-font_size_axis = 13
-font_size_tick = 11
-font_size_legend = 9
-
-x_label = "X-axis, example time $t$ / s"
-y_label = "Y-axis, example $f(t)$"
-
-x_lim = [np.min(x_data), np.max(x_data)]
-y_lim = [np.min(y_data), np.max(y_data)]
-
-grid_major = True
-grid_minor = False
-legend_on = True
+x_lim = [761.6,766.8] #[np.min(x_data), np.max(x_data)]
+y_lim = [0,55] #[np.min(y_data), np.max(y_data)]
 
 f.set_LaTeX_and_CMU(True) #must be before plotting
+fig, axs = plt.subplots(nrows=1, ncols=1, figsize=(16/2.54, 9/2.54), sharex=False, sharey=False)
 
-# PLOT # 
 
-# Create figure on which axis lives, one axis per subplot
-fig, axs = plt.subplots(nrows=1, ncols=1, figsize=(fig_width_cm/2.54, fig_height_cm/2.54), sharex=False, sharey=False)
-
-# Plot your data (axs.plot, .errorbar, .hist, ...)
+# Plot
 axs.plot(x_data, y_data, linewidth=1.5, linestyle='', color='k', marker='o', markersize='4.5', label='Measured data')
 axs.plot(fit_x, fit_y, color='k', linestyle='-', label='Linear fit')
+axs.fill_between(fit_x, fit_y + 2*fit_y_oneSigma, fit_y - 2*fit_y_oneSigma, color='b', alpha=0.25, label=f'Prediction interval ($2\\sigma$)')
+#axs.errorbar(763.8, 21.9, 1.5, label='Unknown concentration', color='r', capsize=2.5, marker='')
 
-# Settings for each axis (axs)
-f.set_font_size(axis=font_size_axis, tick=font_size_tick, legend=font_size_legend)
+# Settings for each axis
+f.set_font_size(axis=13, tick=11, legend=9)
 f.set_axis_scale(   axs, xScale_string='linear', yScale_string='linear')
-f.set_axis_labels(  axs, x_label=x_label, y_label=y_label)
+f.set_axis_labels(  axs, x_label="LSPR peak wavelength / nm", y_label="Concentration (\%)")
 f.set_axis_invert(  axs, x_invert=False, y_invert=False)
 f.set_axis_limits(  axs, x_lim[0], x_lim[1], y_lim[0], y_lim[1])
-f.set_grid(         axs, grid_major_on=grid_major, grid_major_linewidth=0.7, grid_minor_on=grid_minor, grid_minor_linewidth=0.3) # set_grid must be after set_axis_scale for some reason (at least with 'log')
-f.set_legend(       axs, legend_on=legend_on, alpha=1.0, location='best')
+f.set_grid(         axs, grid_major_on=False, grid_major_linewidth=0.7, grid_minor_on=False, grid_minor_linewidth=0.3) # set_grid must be after set_axis_scale for some reason (at least with 'log')
+f.set_legend(       axs, legend_on=True, alpha=1.0, location='best')
 
-#loc = plticker.MultipleLocator(base=5) # this locator puts ticks at regular intervals determined by base
+
+#loc = plticker.MultipleLocator(base=5) # this locator puts ticks at regular intervals
 #axs.xaxis.set_major_locator(loc)
 
 f.align_labels(fig)
